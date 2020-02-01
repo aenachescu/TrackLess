a567047e03032df0deb31d1d4161d808c317d46e
aenachescu <alinenachescu033@gmail.com>
2020-02-01 23:39:20 +0200

    added more blacklisted parameters; added support to remove parameters by hostname

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TrackLess
 Remove tracking parameters from URLs.
+
+TODO:
+
+jumpid & dclid
+https://www8.hp.com/ro/ro/printers/Smart-Tank.html?jumpid=sc_1pekgu3s9a&/dm:_N5851.152606FACEBOOK.COM2_265141310_460320822_127434391_6869598&dclid=CjkKEQiA4NTxBRCoo_KThJ_r8rsBEiQAQx8zv0rCoMDpH-Mk-8nAkIcKPxrNArotIhKU3czLsq2YL2bw_wcB
+
+blade & hulk & ultron
+https://www.elastic.co/webinars/observability-for-time-series-with-kibana-visual-builder?blade=facebook&hulk=cpc&ultron=kibana-interest
--- js/background.js
+++ js/background.js
@@ -12,26 +12,109 @@ if (typeof chrome !== "undefined") {
 // core
 //
 
-const g_blackListedParameters = [
-    "fbclid",
-    "gclid",
-    "utm_medium",
-    "utm_source",
-    "utm_content",
-    "utm_serial",
-    "utm_campaign",
-    "utm_term"
-];
+const g_blackListedParameters = {
+    // facebook
+    "fbclid"            : true,
+    "feature"           : true, // TODO: research more about this parameter, sometimes it could be legit
+    "ad_id"             : true,
+    "adset_id"          : true,
+    "campaign_id"       : true,
+    "ad_name"           : true,
+    "adset_name"        : true,
+    "campaign_name"     : true,
+    "placement"         : true,
+    "site_source_name"  : true,
 
-function removeTrackingParameters(strUrl)
+    // google
+    "gclid"             : true,
+    "utm_medium"        : true,
+    "utm_source"        : true,
+    "utm_content"       : true,
+    "utm_serial"        : true,
+    "utm_campaign"      : true,
+    "utm_term"          : true,
+    "utm_id"            : true,
+
+    // hubspot
+    "hsa_cam"           : true,
+    "hsa_grp"           : true,
+    "hsa_mt"            : true,
+    "hsa_src"           : true,
+    "hsa_ad"            : true,
+    "hsa_acc"           : true,
+    "hsa_net"           : true,
+    "hsa_kw"            : true,
+    "hsa_tgt"           : true,
+    "hsa_ver"           : true,
+
+    // ns* (who the hell adds these parameters???)
+    "ns_campaign"       : true,
+    "ns_mchannel"       : true,
+    "ns_source"         : true,
+    "ocid"              : true, // TODO: research more about this parameter, sometimes it could be legit
+};
+
+const g_blackListedParametersByHostname = {
+    "www.facebook.com": {
+        "parameters" : {
+            "__xts__[0]"        : true,
+            "__xts__%5B0%5D"    : true,
+            "__tn__"            : true,
+            "eid"               : true,
+            "hc_ref"            : true,
+            "fref"              : true,
+            "ref"               : true,
+        },
+        "originHostname": "www.facebook.com",
+        "checkOriginHostname" : false,
+        "removeGlobalBlacklistedParameters": true,
+    },
+};
+
+function removeTrackingParameters(strUrl, strOriginUrl)
 {
     var modified = false;
+    var removeGlobalBlacklistedParameters = true;
     const url = new URL(strUrl);
+    var parameters = [...url.searchParams.keys()];
 
-    for (const parameter of g_blackListedParameters) {
-        if (url.searchParams.get(parameter)) {
-            url.searchParams.delete(parameter);
-            modified = true;
+    if (typeof g_blackListedParametersByHostname[url.hostname] !== 'undefined') {
+        const hostDetails = g_blackListedParametersByHostname[url.hostname];
+        var removeParametersByHostname = true;
+
+        if (hostDetails.checkOriginHostname === true) {
+            if (strOriginUrl === undefined) {
+                removeParametersByHostname = false;
+            } else {
+                const originUrl = new URL(strOriginUrl);
+                if (hostDetails.originHostname !== originUrl.hostname) {
+                    removeParametersByHostname = false;
+                }
+            }
+        }
+
+        if (removeParametersByHostname === true) {
+            for (const parameter of parameters) {
+                if (typeof hostDetails.parameters[parameter] !== 'undefined') {
+                    url.searchParams.delete(parameter);
+                    modified = true;
+                }
+            }
+
+            removeGlobalBlacklistedParameters = hostDetails.removeGlobalBlacklistedParameters;
+
+            if (modified === true) {
+                parameters = [...url.searchParams.keys()];
+            }
+        }
+    }
+
+    if (removeGlobalBlacklistedParameters === true) {
+        for (const parameter of parameters) {
+            if (typeof g_blackListedParameters[parameter] !== 'undefined') {
+                url.searchParams.delete(parameter);
+                modified = true;
+            }
         }
     }
 
@@ -46,7 +129,6 @@ function removeTrackingParameters(strUrl)
         modified: false
     };
 }
-
 
 //
 // settings
@@ -109,10 +191,16 @@ let g_proxyEnabled = false;
 function onBeforeRequestCbk(details)
 {
     if (details.url) {
-        var result = removeTrackingParameters(details.url);
+        var strOriginUrl;
+        if (isChrome === true) {
+            strOriginUrl = details.initiator;
+        } else {
+            strOriginUrl = details.originUrl;
+        }
+
+        var result = removeTrackingParameters(details.url, strOriginUrl);
         if (result.modified === true) {
-            // console.info(`original url: ${details.url}`);
-            // console.info(`modified url: ${result.url}`);
+            // console.info(`original url: ${details.url}   -->   modified url: ${result.url}`);
             return {
                 redirectUrl: result.url
             };
